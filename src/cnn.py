e0a024dd6502ad3cd4d3fc9075dc3ec3d8c238a9
Lucas Silva <44052175+LucasBSilva99@users.noreply.github.com>
2022-03-21 14:43:42 +0000

    Update cnn.py

--- src/cnn.py
+++ src/cnn.py
@@ -25,8 +25,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from sklearn.metrics import accuracy_score
 from pytorch_lightning.loggers import TensorBoardLogger
-from sklearn.model_selection import StratifiedGroupKFold
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from torchmetrics import ConfusionMatrix
 
 class SoftOrdering1DCNN(pl.LightningModule):
 
