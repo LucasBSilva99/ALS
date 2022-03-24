7cfbfb071435655fb90d35168ae357b50ddba26a
Lucas Silva <44052175+LucasBSilva99@users.noreply.github.com>
2022-03-24 11:01:58 +0000

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -2,19 +2,19 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 class AlsDataset(Dataset):
-  def __init__(self, X, y):
-    #data loading
-    self.x = torch.tensor(X.values, dtype=torch.float)
-    self.y = torch.tensor(y.values, dtype=torch.float)
-    self.n_samples = X.shape[0]
+    def __init__(self, X, y):
+      #data loading
+      self.x = torch.tensor(X.values, dtype=torch.float)
+      self.y = torch.tensor(y.values, dtype=torch.float)
+      self.n_samples = X.shape[0]
 
-  def __len__(self):
-    # len(dataset)
-    return self.n_samples
+    def __len__(self):
+      # len(dataset)
+      return self.n_samples
 
-  def __getitem__(self, index):
-    # dataset[0]
-    return self.x[index], self.y[index]
+    def __getitem__(self, index):
+      # dataset[0]
+      return self.x[index], self.y[index]
 
-  def get_label_count(self):
-    return self.y.unique()
+    def get_label_count(self):
+      return self.y.unique()
